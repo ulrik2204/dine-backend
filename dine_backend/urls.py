5107b141c95ccb67c915207963fa72655bcc6fe9
Ulrik Røsby <ulrikro@stud.ntnu.no>
2021-02-18 00:38:27 +0100

    Add single dinner view in api

--- dine_backend/urls.py
+++ dine_backend/urls.py
@@ -1,8 +1,10 @@
 """Urls for this app"""
 
 from django.urls import path
-from .views import DinnerView
+from dine_backend.views import DinnerView, DinnersAllView
 
 urlpatterns = [
-    path('', DinnerView.as_view())
+    path('', DinnersAllView.as_view()),
+    path('<int:pk>/', DinnerView.as_view())
+    # Uses the primary key of the database object (pk) to locate a single Dinner
 ]

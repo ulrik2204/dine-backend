639794da816415adf4bc5af3fd414f4bf2077ca3
Ulrik Røsby <ulrikro@stud.ntnu.no>
2021-03-31 13:51:33 +0200

    Add tests for editing a dinner - TDD

--- dine_backend/tests/constants.py
+++ dine_backend/tests/constants.py
@@ -8,18 +8,39 @@ USER_1 = {
     "allergies": [5, 9]
 }
 
+USER_2 = {
+    "username": "test_user2",
+    "first_name": "Noen",
+    "last_name": "Noensen",
+    "address": "Heisannveien",
+    "password": "password",
+    "password2": "password",
+    "allergies": [1, 2, 3]
+}
+
+
 DINNER_1 = {
+    "id": 1,
     "dish": "Carbonara amazing",
-    "cuisine": "Other",
-    "date": "2021-04-19T11:11",
+    "cuisine": "Italiensk",
+    "date": "2021-04-19T11:11Z",
     "location": "Oslo",
-    "allergies": [2, 6]
+    "owner": 1,  # The owner that will be assigned
+    "description": "Dette er en beskrivelse",
+    "allergies": [2, 6],
+    "signed_up_users": [],  # Has to be empty, the request does not care about this one
+    "is_canceled": False
 }
 
 DINNER_2 = {
-    "dish": "Carbonara ",
-    "cuisine": "Other",
-    "date": "2021-04-19T11:11",
+    "id": 2,
+    "dish": "Spaghetti bolognese",
+    "cuisine": "Italiensk",
+    "date": "2021-04-19T11:11Z",
     "location": "Oslo",
-    "allergies": [5]
+    "owner": 1,  # The owner that will be assigned
+    "description": "",
+    "allergies": [5],
+    "signed_up_users": [],  # Has to be empty, the request does not care about this one
+    "is_canceled": False
 }

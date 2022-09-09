812f4a2a89c0628acbf09fe33b363cf6b9514c81
Ulrik Røsby <ulrik@rosby.no>
2022-09-09 19:04:01 +0200

    feat: add script to create dev db

---
+++ scripts/devdb.ps1
@@ -0,0 +1,1 @@
+docker run --name postgresqldb -e POSTGRES_PASSWORD=docker -p 5432:5432 -d postgres
---
+++ scripts/devdb.sh
@@ -0,0 +1,1 @@
+docker run --name postgresqldb -e POSTGRES_PASSWORD=password -p 5432:5432 -d postgres
